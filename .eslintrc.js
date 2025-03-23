ee8012b3d892aea642357a227bd2aaa469d13e05
Muhammed <mamipromax1513@gmail.com>
2025-03-23 18:19:27 +0300

    fix: formatting and linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: "@react-native",
   plugins: ["prettier"],
+  ignorePatterns: ["android/"],
   rules: {
     "prettier/prettier": "error",
     "linebreak-style": ["error", "unix"],
